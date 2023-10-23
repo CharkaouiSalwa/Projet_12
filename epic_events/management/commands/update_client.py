85dadbe42cb5a7d440f178750b3ea47aee94b31f
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-10-23 11:43:27 +0200

    update file update_client.py

--- epic_events/management/commands/update_client.py
+++ epic_events/management/commands/update_client.py
@@ -6,6 +6,15 @@ from .epiceventcommand import EpicEventCommand
 class Command(EpicEventCommand):
     help = 'Mettez à jour les informations d\'un client associé à l\'utilisateur authentifié ayant le rôle "commercial"'
 
+    def add_arguments(self, parser):
+        parser.add_argument('client_id', type=int, help='ID du client à mettre à jour')
+        parser.add_argument('--nom_complet', type=str, help='Nouveau client', required=False)
+        parser.add_argument('--email', type=str, help='Nouvelle adresse mail', required=False)
+        parser.add_argument('--telephone', type=str, help='Nouveau numero de telephone', required=False)
+        parser.add_argument('--nom_entreprise', type=str, help='Nouvelle entreprise', required=False)
+        parser.add_argument('--date_creation', type=str, help='Nouvelle date de création', required=False)
+        parser.add_argument('--derniere_mise_a_jour', type=str, help='Dernière mise à jour', required=False)
+
     def execute_authenticated_command(self, *args, **options):
         # Lisez le token depuis le fichier 'token.txt'
         with open('token.txt', 'r') as file:
@@ -16,25 +25,32 @@ class Command(EpicEventCommand):
         if user and user.role == 'commercial':
             self.stdout.write(self.style.SUCCESS(f'- Bienvenue, {user.username}'))
 
-            # Demandez à l'utilisateur de saisir l'ID du client à mettre à jour
-            client_id = input('ID du client à mettre à jour : ')
+            client_id = options['client_id']
 
-            # Vérifiez si le client existe
-            client = Client.objects.filter(id=client_id).first()
-            if not client:
+            try:
+                # Vérifiez si le client existe
+                client = Client.objects.get(id=client_id)
+
+                # Mettez à jour les informations du client
+                if options['nom_complet']:
+                    client.nom_complet = options['nom_complet']
+                if options['email']:
+                    client.email = options['email']
+                if options['telephone']:
+                    client.telephone = options['telephone']
+                if options['nom_entreprise']:
+                    client.nom_entreprise = options['nom_entreprise']
+                if options['date_creation']:
+                    client.date_creation = options['date_creation']
+                if options['derniere_mise_a_jour']:
+                    client.derniere_mise_a_jour = options['derniere_mise_a_jour']
+
+                # Mettez à jour les informations du client
+                client.derniere_mise_a_jour = timezone.now()
+                client.save()
+
+                self.stdout.write(self.style.SUCCESS(f'Les informations du client ont été mises à jour avec succès.'))
+            except Client.DoesNotExist:
                 self.stdout.write(self.style.ERROR("Le client spécifié n'a pas été trouvé."))
-                return
-
-            # Demandez à l'utilisateur de fournir les informations mises à jour pour le client
-            client.nom_complet = input('Nouveau nom complet du client (laissez vide pour ne pas mettre à jour) : ') or client.nom_complet
-            client.email = input('Nouvelle adresse e-mail du client (laissez vide pour ne pas mettre à jour) : ') or client.email
-            client.telephone = input('Nouveau numéro de téléphone du client (laissez vide pour ne pas mettre à jour) : ') or client.telephone
-            client.nom_entreprise = input('Nouveau nom de l\'entreprise du client (laissez vide pour ne pas mettre à jour) : ') or client.nom_entreprise
-
-            # Mettez à jour les informations du client
-            client.derniere_mise_a_jour = timezone.now()
-            client.save()
-
-            self.stdout.write(self.style.SUCCESS(f'Les informations du client ont été mises à jour avec succès.'))
         else:
             self.stdout.write(self.style.ERROR("Utilisateur non trouvé, non authentifié ou n'a pas le rôle 'commercial'. Veuillez vous connecter et réessayer."))
